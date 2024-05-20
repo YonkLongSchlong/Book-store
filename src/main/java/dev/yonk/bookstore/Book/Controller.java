df92fa51a696b26be39c364b35f131e9a9c2d8eb
YonkLongSchlong <beeline3022@gmail.com>
2024-05-20 23:18:25 +0700

    push

--- src/main/java/dev/yonk/bookstore/Book/Controller.java
+++ src/main/java/dev/yonk/bookstore/Book/Controller.java
@@ -3,9 +3,7 @@ package dev.yonk.bookstore.Book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +18,43 @@ public class Controller {
     public ResponseEntity<List<Book>> getAllBook () {
         return new ResponseEntity<>(service.getAllBooks(), HttpStatus.OK);
     }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addBook(@RequestBody Book book) {
+        try {
+            return new ResponseEntity<>(service.addBook(book), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("books/{id}")
+    public ResponseEntity<?> findById(@PathVariable String id) {
+        try {
+            return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/delete/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable String id) {
+        try {
+            service.deleteBook(id);
+            return new ResponseEntity<>("Delete book successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Book not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/update/{id}")
+    public ResponseEntity<?> updateBook(@PathVariable String id, @RequestBody Book bookUpdate) {
+        try {
+            return new ResponseEntity<>(service.updateBook(id, bookUpdate), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
 }
--- src/main/java/dev/yonk/bookstore/Book/Service.java
+++ src/main/java/dev/yonk/bookstore/Book/Service.java
@@ -12,4 +12,39 @@ public class Service {
     public List<Book> getAllBooks() {
         return repository.findAll();
     }
+
+    public Book addBook(Book book) throws Exception {
+        try {
+            return repository.save(book);
+        } catch (Exception e) {
+            throw new Exception("Error adding new Book");
+        }
+    }
+
+    public Book findById(String id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+    }
+
+    public void deleteBook(String id) {
+         try {
+             repository.deleteById(id);
+         } catch (Exception e) {
+             throw new RuntimeException("Book not found");
+         }
+    }
+
+    public Book updateBook(String id, Book bookUpdate) {
+        try {
+            Book book = repository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Book not found"));
+            book.setName(bookUpdate.getName());
+            book.setAuthor(bookUpdate.getAuthor());
+            book.setQuantity(bookUpdate.getQuantity());
+            book.setPrice(bookUpdate.getPrice());
+            return repository.save(book);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't update book");
+        }
+    }
 }
